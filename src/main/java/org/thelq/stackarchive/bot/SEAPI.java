15e5bbce120b500f5d16688da00eb1f0c27a1e5f
Leon Blakey <lord.quackstar@gmail.com>
2013-02-24 15:32:36 -0500

    Created a basic SEAPI utility class

---
+++ src/main/java/org/thelq/stackarchive/bot/SEAPI.java
@@ -0,0 +1,68 @@
+package org.thelq.stackarchive.bot;
+
+import java.io.IOException;
+import java.util.Properties;
+import net.sf.json.JSONObject;
+import net.sf.json.JSONSerializer;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.fluent.Request;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.ContentEncodingHttpClient;
+import org.apache.http.impl.client.DecompressingHttpClient;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
+/**
+ *
+ * @author Leon
+ */
+public class SEAPI {
+	protected final String seApiKey;
+	protected static SEAPI instance;
+	protected HttpClient httpclient = new DecompressingHttpClient(new DefaultHttpClient());
+
+	static {
+		try {
+			instance = new SEAPI();
+		} catch (IOException ex) {
+			ex.printStackTrace();
+			System.exit(2);
+		}
+	}
+
+	public static SEAPI get() {
+		return instance;
+	}
+
+	public SEAPI() throws IOException {
+		Properties config = new Properties();
+		config.load(getClass().getResourceAsStream("/config.properties"));
+		seApiKey = config.getProperty("se_api_key");
+	}
+
+	public void getRecent() throws Exception {
+		querySE("posts", "stackoverflow");
+	}
+
+	protected JSONObject querySE(String method, String site, String... options) throws Exception {
+		HttpGet httpGet = null;
+		try {
+			String url = "https://api.stackexchange.com/2.1/" + method + "?key=" + seApiKey + "&site=" + site;
+			for (String curOption : options)
+				url += "&" + curOption;
+			System.out.println("URL: " + url);
+			//TODO: Figure out how to handle errors with different status codes (causes Exception)
+			httpGet = new HttpGet(url);
+			HttpResponse response1 = httpclient.execute(httpGet);
+			String rawResponse = EntityUtils.toString(response1.getEntity());
+			return (JSONObject) JSONSerializer.toJSON(rawResponse);
+		} catch (Exception e) {
+			throw new Exception("Can't query recent questions", e);
+		} finally {
+			if (httpGet != null)
+				httpGet.releaseConnection();
+		}
+	}
+}
