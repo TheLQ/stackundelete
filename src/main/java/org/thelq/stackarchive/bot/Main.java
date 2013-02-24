6332615e1d64f869e2f577d7e0192d472b9b3140
Leon Blakey <lord.quackstar@gmail.com>
2013-02-24 15:33:22 -0500

    Renamed App to Main

---
+++ src/main/java/org/thelq/stackarchive/bot/Main.java
@@ -0,0 +1,11 @@
+package org.thelq.stackarchive.bot;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main {
+	public static void main(String[] args) throws Exception {
+		SEAPI.get().getRecent();
+	}
+}
